9a00114e485fbb3ef988f6a294247ab6071ee6d5
unknown <natalishapov@live.com>
2016-10-26 17:02:29 +0300

    menu

--- app/js/js-modules/main.js
+++ app/js/js-modules/main.js
@@ -1,6 +1,12 @@
 //= js-assets/variables
 
 $(document).ready(function() {
+	$('.header-toggle__btn').on('click', function(event) {
+		event.preventDefault();
+		/* Act on the event */
+		$(this).toggleClass('active');
+		$('.header-nav__nav').toggleClass('active');
+	});
 	$('.slider-list').slick({
 		slidesToShow: 1,
 		dots: true,
--- app/js/main.js
+++ app/js/main.js
@@ -1,6 +1,12 @@
 
 
 $(document).ready(function() {
+	$('.header-toggle__btn').on('click', function(event) {
+		event.preventDefault();
+		/* Act on the event */
+		$(this).toggleClass('active');
+		$('.header-nav__nav').toggleClass('active');
+	});
 	$('.slider-list').slick({
 		slidesToShow: 1,
 		dots: true,
